97929b7f0ec6b1c77f307bee0d9eb0ff0efba7ab
Chris Davis <contact@nthstate.com>
2022-10-09 14:50:42 +0100

    first commit

---
+++ ProductAnalyticsDemo/ProductAnalyticsDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ProductAnalyticsDemoApp.swift
+//  ProductAnalyticsDemo
+//
+//  Created by Chris Davis on 09/10/2022.
+//
+
+import SwiftUI
+import ProductAnalyticsCore
+
+@main
+struct ProductAnalyticsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
